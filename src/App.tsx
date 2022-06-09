12f30fba7a9662f0029a9df63c6c4e28144fbd98
Vinicius <viniciusmartinsoprb.02@gmail.com>
2022-06-09 00:58:21 -0300

    one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppRoutes } from "./Route"
 import "./styles/global.sass"
+import { Navbar } from "./components/Navbar"
 
 export function App() {
   return (
     <>
+      <Navbar/>
       <AppRoutes></AppRoutes>
     </>
   )
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.sass"
+export function Navbar(){
+    return (
+        <>
+           <ul className={styles.lista}>
+               <li>ASDASD</li>
+               <li>ASDASD</li>
+               <li>ASDASD</li>
+               <li>ASDASD</li>
+            </ul> 
+        </>
+    )
+}
