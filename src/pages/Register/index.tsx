e7f7d2def24d25f0c3b77e5a171ee814b252ebff
leonardoreizz <amarantee.reis@gmail.com>
2022-06-09 10:10:14 -0300

    consoleactive

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+export function Register() {
+    useEffect(()=>{
+        document.title = "System - Cadastro"
+    })
+    return (
+        <>
+
+        </>
+    )
+}
