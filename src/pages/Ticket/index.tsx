8e709d354f771a5eeb94d6b586b267187ee33cb8
Vinicius <viniciusmartinsoprb.02@gmail.com>
2022-06-14 20:34:39 -0300

    xc

--- src/pages/Ticket/index.tsx
+++ src/pages/Ticket/index.tsx
@@ -1,9 +1,17 @@
+import styles  from "./styles.module.sass"
 
 export function Ticket () {
     return (
         <>
-
-
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.limit}>
+                        <div className={styles.boxTicket}>
+                            <h2>@</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
